693d37ec718c31e27dfd26b12134bde33f666184
oneblueandred <nolanbailey21@gmail.com>
2025-05-02 18:33:38 -0700

    lambda

--- add.py
+++ add.py
@@ -1,9 +1,9 @@
 def add(folder, num1, num2, output):
     num3 = num1 + num2
-    with open("output.txt", 'w') as f:
+    with open("/tmp/output.txt", 'w') as f:
         f.write(str(num3))
     try:
-        FaaSr_py.faasr_put_file(local_file="output.txt", remote_folder=folder, remote_file=output)   
+        FaaSr_py.faasr_put_file(local_file="/tmp/output.txt", remote_folder=folder, remote_file=output)   
     except Exception as e:
         print(f"Exception with faasr_put_file: {str(e)}")
     print(f"add: {num1} + {num2} = {num3}")
