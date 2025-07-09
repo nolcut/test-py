6fab89d2e031c1d4a3aa7c0f5c2f673b92a9b4b4
oneblueandred <nolanbailey21@gmail.com>
2025-07-09 14:30:22 -0700

    args

--- http_request.py
+++ http_request.py
@@ -1,3 +1,5 @@
 def http_request():
     print("test action")
-    r = requests.post("http://127.0.0.1:8000/faasr-action")
+    test_json = {"ProcedureID": "test",
+                 "Arguments": {"arg1": 1, "arg2": 2}}
+    r = requests.post("http://127.0.0.1:8000/faasr-action", json=test_json)
