1dce806f747dcbe06d0dd8ac3c50646840d8fc03
oneblueandred <nolanbailey21@gmail.com>
2025-07-10 02:18:40 -0700

    wrapper

--- add_rpc.py
+++ add_rpc.py
@@ -3,13 +3,7 @@ def add_rpc(folder, num1, num2, output):
     with open("/tmp/output.txt", 'w') as f:
         f.write(str(num3))
     try:
-        request_json = {
-            "ProcedureID": "faasr_put_file",
-            "Arguments": {"local_file": "/tmp/output.txt",
-                          "remote_folder": folder,
-                          "remote_file": output}
-        }
-        r = requests.post("http://127.0.0.1:8000/faasr-action", json=request_json)
+        faasr_put_file(local_file="/tmp/output.txt", remote_folder="folder", remote_file="output")
     except Exception as e:
         print(f"Exception with faasr_put_file: {str(e)}")
     print(f"add: {num1} + {num2} = {num3}")
